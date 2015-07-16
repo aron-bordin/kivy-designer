263fce42cb29a82368bc785ef26e787fb901cfa9
Aron Bordin <aron.bordin@gmail.com>
2015-07-16 20:01:49 -0300

    save view settings

--- designer/uix/modules_contview.py
+++ designer/uix/modules_contview.py
@@ -1,3 +1,4 @@
+from kivy.app import App
 from kivy.properties import ObjectProperty, Clock, partial
 from kivy.uix.actionbar import ContextualActionView
 from kivy.modules import screen
@@ -41,22 +42,52 @@ class ModScreenContView(ContextualActionView):
 
     __events__ = ('on_run', )
 
+    designer = ObjectProperty(None)
+    '''Instance of Desiger
+    '''
+
     def __init__(self, **kwargs):
         super(ModScreenContView, self).__init__(**kwargs)
 
         # populate emulation devices
         devices = self.ids.module_screen_device
-        group = 'screen_device'
+
+        self.designer = App.get_running_app().root
+        config = self.designer.designer_settings.config_parser
+
+        # load the default values
+        saved_device = config.getdefault('internal', 'mod_screen_device', '')
+        saved_orientation = config.getdefault('internal',
+                                              'mod_screen_orientation', '')
+        saved_scale = config.getdefault('internal', 'mod_screen_scale', '')
 
         first = True
+        first_btn = None
         for device in sorted(screen.devices):
-            btn = DesignerActionProfileCheck(group=group,
-                                             allow_no_selection=False,
-                                             config_key=device)
+            btn = DesignerActionProfileCheck(group='mod_screen_device',
+                            allow_no_selection=False, config_key=device)
             btn.text = screen.devices[device][0]
-            btn.checkbox_active = first
-            first = False
+            btn.bind(on_active=self.on_module_settings)
+
+            if first:
+                btn.checkbox_active = first
+                first_btn = btn
+                first = False
+
+            if device == saved_device:
+                first_btn.checkbox_active = False
+                btn.checkbox_active = True
+            else:
+                btn.checkbox_active = False
+
             devices.add_widget(btn)
+        for orientation in self.ids.module_screen_orientation.list_action_item:
+            if orientation.config_key == saved_orientation:
+                orientation.checkbox_active = True
+
+        for scale in self.ids.module_screen_scale.list_action_item:
+            if scale.config_key == saved_scale:
+                scale.checkbox_active = True
 
     def on_run_press(self, *args):
         '''Run button pressed. Analyze settings and dispatch ModulesContView
@@ -89,3 +120,15 @@ class ModScreenContView(ContextualActionView):
         '''Event handler for on_run
         '''
         pass
+
+    def on_module_settings(self, instance, *args):
+        '''Event handle to save Screen Module settings when a different
+        option is selected
+        '''
+        if instance.checkbox.active:
+            self.designer.designer_settings.config_parser.set(
+                'internal',
+                instance.group,
+                instance.config_key
+            )
+            self.designer.designer_settings.config_parser.write()
